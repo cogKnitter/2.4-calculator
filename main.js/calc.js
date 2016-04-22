2b2999b7b88a547940be2ec16caef3592b69bb44
Mariah Tree <mariah.tree@gmail.com>
2016-04-22 15:54:07 -0500

    setting up DOM

---
+++ main.js/calc.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(e){
+
+  var calcDisp = document.querySelector("[data-js='calc-output']")
+  var numClick = document.querySelector("[data-js='num-press']");
+  var operatorClick = document.querySelector("[data-js='op-press']")
+
+  }
+
+})
